a6db126ec2aae0e8f164690bf81adf1c7b4caa32
Valeria <valeria.rocha@despegar.com>
2019-11-28 15:16:55 -0300

    read block

--- mainMemory.c
+++ mainMemory.c
@@ -1,1 +1,10 @@
 #include "mainMemory.h"
+
+void main_memory_create(main_memory_t *self) {}
+
+void main_memory_destroy(main_memory_t *self);
+
+char *read_block_num(main_memory_t *self, unsigned int blocknum)
+{
+    return self->memoryTable[blocknum]; //adjust blocknum
+}
--- mainMemory.h
+++ mainMemory.h
@@ -1,14 +1,17 @@
 #ifndef ORGACOMPU_TP2_MAINMEMORY_H
 #define ORGACOMPU_TP2_MAINMEMORY_H
 
-typedef struct {
-    char* memoryTable;
-}main_memory_t;
+typedef struct
+{
+    char *memoryTable;
+} main_memory_t;
 
-void main_memory_create(main_memory_t* self);
+void main_memory_create(main_memory_t *self);
 
-void main_memory_destroy(main_memory_t* self);
+void main_memory_destroy(main_memory_t *self);
 
-void main_memory_init(main_memory_t* self);
+void main_memory_init(main_memory_t *self);
+
+char *read_block_num(main_memory_t *self, unsigned int blocknum);
 
 #endif //ORGACOMPU_TP2_MAINMEMORY_H
